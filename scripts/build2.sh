9eac6b2d07381cadc5ca50baba78b3f3f0b0318f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-12-11 18:28:00 -0800

    Update patches to enable javac warnings

--- scripts/build2.sh
+++ scripts/build2.sh
@@ -13,9 +13,9 @@
 INSTALL_DIR=$(dirname $(readlink -f "$BASH_SOURCE"))
 PATCH_DIR="${INSTALL_DIR}/../patches/all-files-warn-on-reflection"
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-    1>&2 echo "usage: `basename $0` <clojure-git-tag> <log-dir>"
+    1>&2 echo "usage: `basename $0` <clojure-git-tag>"
     exit 1
 fi
 
@@ -26,9 +26,40 @@ fi
 # e.g. clojure-1.10.1
 
 CLJ_VERSION="$1"
-LOG_DIR="$2"
 
-LOGF="${LOG_DIR}/build-log-${CLJ_VERSION}.txt"
 
-${INSTALL_DIR}/build2.sh "${CLJ_VERSION}" |& tee "${LOGF}"
-${INSTALL_DIR}/analyze-log.sh "${LOGF}"
+# Show versions of everything relevant to the build
+
+set -ex
+uname -a
+lsb_release -a
+java -version
+mvn -version
+
+if [ ! -d clojure ]
+then
+    git clone https://github.com/clojure/clojure
+fi
+
+cp -pr clojure "${CLJ_VERSION}"
+cd "${CLJ_VERSION}"
+
+git checkout "${CLJ_VERSION}"
+patch -p1 < "${PATCH_DIR}/${CLJ_VERSION}.patch"
+# Check to see if any .clj files do NOT have any occurrences of:
+#     (set! *warn-on-reflection* true)
+# All of them should.
+set +ex
+echo "Clojure source files in src/clj/clojure, or subdirectories, with"
+echo "0 occurrences of (set! *warn-on-reflection* true):"
+echo "----------------------------------------------------------------------"
+find src/clj/clojure -name '*.clj' | xargs grep -c '(set! \*warn-on-reflection\* true)' | grep ':0$'
+echo "----------------------------------------------------------------------"
+set -ex
+
+mvn clean
+mvn install
+EXIT_STATUS=$?
+
+set +ex
+echo "mvn install exit status: ${EXIT_STATUS}"

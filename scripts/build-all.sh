7cceae679e6b3c001c8a1eabc01a169517cd3c4b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-01-16 09:14:56 -0800

    Add Clojure 1.10.2-rc2 to list of versions in build-all.sh

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,7 +5,8 @@ for clj_ver in \
     clojure-1.9.0 \
     clojure-1.10.0 \
     clojure-1.10.1 \
-    clojure-1.10.2-rc1
+    clojure-1.10.2-rc1 \
+    clojure-1.10.2-rc2
 do
     ./scripts/build.sh ${clj_ver} logs
 done

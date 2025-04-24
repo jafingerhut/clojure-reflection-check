d35a7cbba2c702f4ef3c0c177a7e666f3db41aa6
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-04-24 20:31:00 +0000

    Add support for checking Clojure source versions 1.11.4, 1.12.0
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,8 +5,8 @@ for clj_ver in \
     clojure-1.9.0 \
     clojure-1.10.0 \
     clojure-1.10.1 \
-    clojure-1.10.2-rc1 \
-    clojure-1.10.2-rc2
+    clojure-1.11.4 \
+    clojure-1.12.0
 do
     ./scripts/build.sh ${clj_ver} logs
 done
